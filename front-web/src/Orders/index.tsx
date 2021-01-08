68e803ad84f186affc868179cb618998228d6d1f
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-01-08 14:25:11 -0300

    Add routes from react-router-dom

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+import './styles.css';
+
+function Orders() {
+    return (
+        <div>
+            <h1>Orders</h1>
+        </div>
+    )
+}
+
+export default Orders;
